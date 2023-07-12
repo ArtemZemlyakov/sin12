a326a9d75cd1d24e4bd8a8cb809e60d0ba73c8da
ArtemZemlyakov <139268188+ArtemZemlyakov@users.noreply.github.com>
2023-07-12 23:26:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// “JavaScript-код с созданием переменных”
+/* “Автор:”
+ Artem Zemlyakov*/
+
+ let myName = 'Artem';
+ let mySurname = 'Zemlyakov';
+ let myFavoriteDrink =' Beer';
+ let myFavoriteAnimal = 'dog';
+ let myFavoriteProgrammingLanguage = 'JS';
+ console.log(myName);
+ console.log(mySurname);
+ console.log(myFavoriteDrink);
+ console.log(myFavoriteAnimal);
+ console.log(myFavoriteProgrammingLanguage);
